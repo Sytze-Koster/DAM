eeee25a1514d04e459890ce7cee285001698a217
Bert-Jan Verwer <bertjan@bever.io>
2016-08-30 12:53:34 +0200

    Solved an issue where there was a 500-error when the page was not found in production without debugging.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,7 +68,7 @@
         </div>
     @endif
 
-    @if($errors->has())
+    @if(isset($errors) && $errors->has())
         <div class="notification -error">
             <div class="container">
                 <i class="icon icon-cancel"></i> {{ trans('dam.general.error.validation') }}
